1a01f8d34b1255ceb3ef661c41f1df297bf40e4d
Ryan Kendrick <ryan@kendrickfamily.info>
2016-01-11 14:44:59 +0000

    Added Interpreter To Code College

---
+++ Code College/Code College/Interpreter/Equality.cs
@@ -0,0 +1,13 @@
+﻿namespace Code_College.Interpreter
+{
+    internal class Equality
+    {
+        public bool Eq()
+        {
+        }
+
+        public bool Ne()
+        {
+        }
+    }
+}
---
+++ Code College/Code College/Interpreter/Interpreter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Code_College.Interpreter
+{
+    public class Interpreter
+    {
+        public void Interpret(string Code)
+        {
+
+        }
+    }
+}
