102d3a94503a260bfca40f005595b7fa8629da8d
Ryan Kendrick <ryan@kendrickfamily.info>
2016-03-31 00:05:16 +0100

    AddExercise Redirect If Not Admin

--- Code College/Code College/Exercises/Exercise 4.ex
+++ Code College/Code College/Exercises/Exercise 4.ex
@@ -7,7 +7,7 @@ Introducing If Statements
 [ExDescription]
 In many programming languages, including Lua, we use if statements to tell the computer what we want to happen next, depending on a certain condition. In the editor, there is an example of an if statement.
 
-In the example, change the word "condition" to 'num < 15'.
+In the editor, change the word "condition" to 'num < 15'.
 
 [ExCodeTemplate]
 num = 5 * 2\r\rif condition then\r\tprint("num is less than 15.")\rend
