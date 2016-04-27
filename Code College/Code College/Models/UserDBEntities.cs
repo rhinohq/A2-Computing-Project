5e75967a90113bb6f51e2dbcecee690cef9ad34e
Ryan Kendrick <ryan@kendrickfamily.info>
2016-04-27 01:48:08 +0100

    Misc Changes

--- Code College/Code College/Models/UserDBEntities.cs
+++ Code College/Code College/Models/UserDBEntities.cs
@@ -6,14 +6,12 @@ namespace Code_College.Models
     // Database context for user database
     public class User
     {
-        public string Email { get; set; }
-
         [Key]
         public string Name { get; set; }
-
+        public string Username { get; set; }
+        public string Email { get; set; }
         public string PasswordHash { get; set; }
         public int UserLevel { get; set; }
-        public string Username { get; set; }
     }
 
     public class UserDBEntities : DbContext
